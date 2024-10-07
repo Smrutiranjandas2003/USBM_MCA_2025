bf492c1acf7008bb067daa28c05a6e62475fea86
benni <rsmruti814@gmail.com>
2024-10-07 12:03:51 +0530

    string task added

---
+++ frontend/javascript/class1/index.js
@@ -0,0 +1,11 @@
+console.log("benni")
+let a = 9.427;
+let pi = 3.1415;
+let add = a + pi;
+let subtract = a - pi;
+let multiply = a * pi;
+let divide = a / pi;
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
---
+++ frontend/javascript/class1/notes.md
@@ -0,0 +1,8 @@
+#how to push github
+
+step 1 :git add
+step 2:git commit -m "ur commit message"
+step 3:clear
+
+main resource then type the command 
+git pull
