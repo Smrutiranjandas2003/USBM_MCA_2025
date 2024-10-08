bee6948255b4884675bafe0518d405b4c49f2ea4
benni <rsmruti814@gmail.com>
2024-10-08 11:57:23 +0530

    class3

---
+++ frontend/javascript/class1/math.js
@@ -0,0 +1,33 @@
+//let a = 89;
+//let b = 43;
+//let result = a + b;
+//let even = a%2;
+//console.log(!5 == "5");
+
+let  total_bill = 1000;
+if (total_bill > 750) {
+    console.log("discount");
+}
+    else{
+        console.log("no discount");
+    }
+
+let age = 21;
+
+if (age >= 18) {
+    console.log("adults");
+}
+else if (age >=60){
+    console.log("seniors");
+}
+else {
+    console.log("children");
+}
+
+
+let age1 = 23;
+let gender = "male";
+
+if (age1 >= 18 && gender === "male") {
+    console.log("u can marry");
+}
