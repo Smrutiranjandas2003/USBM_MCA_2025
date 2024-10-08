275e10051d8eb0eb9962d26d3c4de43c29735645
benni <rsmruti814@gmail.com>
2024-10-08 12:27:58 +0530

    class3

--- frontend/javascript/class1/math.js
+++ frontend/javascript/class1/math.js
@@ -28,6 +28,86 @@ else {
 let age1 = 23;
 let gender = "male";
 
-if (age1 >= 18 && gender === "male") {
+if (age1 >= 23 && gender === "male") {
     console.log("u can marry");
+}
+
+let a = "d";
+if (a ==="a" || a === "e" || a === "i" || a === "o"|| a === "u"  )
+{
+    console.log("vowel");
+}
+else{
+    console.log("not a vowel");
+}
+
+
+//switch case vowel
+
+let char = "u";
+
+switch (char) {
+ case "a" :
+    console.log("vowel");
+    break;
+    case "e" :
+        console.log("vowel");
+        break;
+    case "i" :
+        console.log("vowel");
+        break;
+    case "o" :
+        console.log("vowel");
+        break;
+    case "u" :
+        console.log("vowel");
+        break;
+    default:    
+    console.log("not a vowel");
+}   
+
+
+
+//month
+let num = 12;
+switch(num){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
 }
