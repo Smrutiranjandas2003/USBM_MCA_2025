8bb479d462ff746ac9a2c646983b282ba2a7d3f1
benni <rsmruti814@gmail.com>
2024-10-15 10:28:15 +0530

    class2 added

---
+++ frontend/javascript/class-4/notes/math.js
@@ -0,0 +1,21 @@
+let n1 = 25;
+let n2 = 35.1555;
+let add = n1 + n2;
+console.log(add);
+
+console.log(Math.round(add));
+console.log(Math.PI);
+console.log(Math.round(.546464));
+
+console.log(n1**2);
+console.log(Math.sqrt(n1));
+console.log(Math.pow(35,3));
+console.log(Math.ceil(n2));
+console.log(Math.floor(n2));
+console.log(Math.round(8.5));
+
+console.log(Math.max(25,445,54,55,555));
+
+console.log(Math.abs(-0.0005));
+
+console.log(Math.random());
