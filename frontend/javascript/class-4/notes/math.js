8e4247992896a1758072794bbf3ada7875741fa3
benni <rsmruti814@gmail.com>
2024-10-15 21:12:19 +0530

    class4added

--- frontend/javascript/class-4/notes/math.js
+++ frontend/javascript/class-4/notes/math.js
@@ -1,21 +1,45 @@
-let n1 = 25;
-let n2 = 35.1555;
-let add = n1 + n2;
-console.log(add);
+// let n1 = 25;
+// let n2 = 35.1555;
+// let add = n1 + n2;
+// console.log(add);
 
-console.log(Math.round(add));
-console.log(Math.PI);
-console.log(Math.round(.546464));
+// console.log(Math.round(add));
+// console.log(Math.PI);
+// console.log(Math.round(.546464));
 
-console.log(n1**2);
-console.log(Math.sqrt(n1));
-console.log(Math.pow(35,3));
-console.log(Math.ceil(n2));
-console.log(Math.floor(n2));
-console.log(Math.round(8.5));
+// console.log(n1**2);
+// console.log(Math.sqrt(n1));
+// console.log(Math.pow(35,3));
+// console.log(Math.ceil(n2));
+// console.log(Math.floor(n2));
+// console.log(Math.round(8.5));
 
-console.log(Math.max(25,445,54,55,555));
+// console.log(Math.max(25,445,54,55,555));
 
-console.log(Math.abs(-0.0005));
+// console.log(Math.abs(-0.0005));
 
-console.log(Math.random());
+// console.log(Math.random()*(27-5+1)+5);
+// console.log(Math.floor(Math.random()*(27-5+1)+5));
+
+// let date = new Date();
+// console.log(date);
+
+let num = 432.1139;
+console.log(Math.floor(num));//432
+
+let start = 1;
+let end = 100;
+totalnum = (start + end)-1;
+console.log(Math.random()*(totalnum));//43.8880384132024
+
+let num2=432.11123;
+console.log(Math.ceil(num2));//433
+
+console.log(Math.pow(2,3));//8
+
+console.log(Math.PI);//3.1415
+
+console.log(Math.floor(6.75));
+
+console.log(Math.ceil(9.12));
+
