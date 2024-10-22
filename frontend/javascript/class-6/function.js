8c29dd00b48bea9e9096af322ec81717aaeab511
benni <rsmruti814@gmail.com>
2024-10-22 12:28:43 +0530

    function added

---
+++ frontend/javascript/class-6/function.js
@@ -0,0 +1,32 @@
+// // fuction
+
+// printname();
+// function printname(){
+//     console.log("benni");
+//  }
+
+
+//  //arguments & parameters
+
+//  function printnamea(name){
+//     console.log(`my name is ${name}`);
+//  }
+//  printnamea("benni");
+
+//  function printnaam(name,city,age){
+//     console.log(`my name is ${name} and i live in ${city} and i am ${age} years old.`);
+//  }
+//  printnaam("benni","bbsr",22);
+ 
+
+//  function printnam(name,city,age){
+//     return `my name is ${name} and i live in ${city} and i am ${age} years old.`;
+//  }
+// console.log( printnam("benni","bbsr",22));
+
+//arrowfunction
+
+const printNm = (naam) =>{
+    return `my name is ${naam} `;
+}
+console.log(printNm("benni"));
---
+++ frontend/javascript/class-6/note.md
@@ -0,0 +1,16 @@
+//function
+function is a block of reuseable codes for doing some certain tasks
+
+//declaration
+ 
+
+ //print ur name 
+ function printname(){
+    console.log("benni");
+ }
+//arguments & parameters
+
+ function printname(name){
+    console.log("my name is ${name}");
+ }
+ printname("benni");
